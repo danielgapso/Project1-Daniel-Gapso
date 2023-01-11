26b7273461f122256c6296875e3e707b5958cde6
Daniel Gapso <dandan11110000@gmail.com>
2023-01-11 18:22:59 +0200

    first commit

--- js/js.js
+++ js/js.js
@@ -1,26 +1,32 @@
-var myTasks=[];//an array to hold the addNote function note objects
+var myTasks=[];
 
-const addNote = () => { //a function to collect and save the note objects created
-    var note = new Object(); //an object to sum together all the recived values
-    note.taskWrite = document.getElementById("taskWrite").value;//the recived text box text
-    note.taskDate = document.getElementById("taskDate").value;//the recived date
-    note.taskTime = document.getElementById("taskTime").value;//the recived time
-    myTasks.push(note);//adds the object to the array
-    makeTable();//calls the make table function
+const checkForData = () => {
+    myTasks=JSON.parse(localStorage.getItem("myData"));
+    makeTable();
+  };
 
-    document.getElementById("myForm").reset();//resets the form as soon as all the data is collected and the user pressed submit
-};
-const makeTable = () => {//a function that holds the recived values and mapping each one
-    var data = "";//a string of the recived values
-    myTasks.map((item) => {//mapping every recived value and write them down
-      data += `
-               
+const makeTable = () => {
+    const tableData = document.getElementById("mySticky");
+    var toHtml = "";
+    myTasks.map((item) => {
+        toHtml += `
+                
                 <p>${item.taskWrite}</p>
                 <p>${item.taskDate}</p>
-                <p>${item.taskTime}</p>
-                
+                <p>${item.taskTime}</p> 
            `;
     });
-   document.getElementById("mySticky").innerHTML =data;//writes down the recived values
- 
+    tableData.innerHTML = toHtml;
+};
+
+checkForData();
+
+const addNote = () => { 
+    var note = new Object(); 
+    note.taskWrite = document.getElementById("taskWrite").value;
+    note.taskDate = document.getElementById("taskDate").value;
+    note.taskTime = document.getElementById("taskTime").value;
+    myTasks.push(note);
+    document.getElementById("myForm").reset();
+    localStorage.setItem("myData", JSON.stringify(myTasks));
 };
