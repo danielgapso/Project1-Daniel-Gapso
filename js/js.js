8d26cdb5d8f9a8b4ff6a3d5e7562520115153185
Daniel Gapso <dandan11110000@gmail.com>
2023-01-12 18:32:29 +0200

    new project with save

--- js/js.js
+++ js/js.js
@@ -6,27 +6,26 @@ const checkForData = () => {
   };
 
 const makeTable = () => {
-    const tableData = document.getElementById("mySticky");
-    var toHtml = "";
-    myTasks.map((item) => {
-        toHtml += `
-                
-                <p>${item.taskWrite}</p>
-                <p>${item.taskDate}</p>
-                <p>${item.taskTime}</p> 
-           `;
-    });
-    tableData.innerHTML = toHtml;
-};
-
-checkForData();
-
-const addNote = () => { 
-    var note = new Object(); 
+    const note = new Object(); 
     note.taskWrite = document.getElementById("taskWrite").value;
     note.taskDate = document.getElementById("taskDate").value;
     note.taskTime = document.getElementById("taskTime").value;
     myTasks.push(note);
     document.getElementById("myForm").reset();
     localStorage.setItem("myData", JSON.stringify(myTasks));
+
+    const tableData = document.getElementById("mySticky");
+    var toHtml = "";
+    myTasks.map((item) => {
+        toHtml += `
+                <div class="myNote">
+                <span class="material-symbols-outlined" id="closeButton">close</span>
+                <div class="myWrite">${item.taskWrite}</div>
+                <div class="myDate">${item.taskDate}</div>
+                <div class="myTime">${item.taskTime}</div> 
+                </div>
+           `;
+    });
+    tableData.innerHTML = toHtml;
 };
+
