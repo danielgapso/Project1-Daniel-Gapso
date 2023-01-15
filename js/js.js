a099d524baa057f031859abd80f13293c805d004
Daniel Gapso <dandan11110000@gmail.com>
2023-01-15 17:24:28 +0200

    ⚒️

--- js/js.js
+++ js/js.js
@@ -13,32 +13,32 @@ const makeTable = () => {
     myTasks.push(note);
     document.getElementById("myForm").reset();
     localStorage.setItem("myData", JSON.stringify(myTasks));
-    
-
+    newNoteDate()
     const tableData = document.getElementById("mySticky");
     var toHtml = "";
     myTasks.map((item) => {
         toHtml += `
                 <div id="myNote">
-                <span class="material-symbols-outlined" onclick="deleteItem()" id="deleteButton">close</span>
+                <span class="material-symbols-outlined" onclick="removeNote()" id="deleteButton">close</span>
                 <div class="myWrite">${item.taskWrite}</div>
-                <div class="myDate">${item.taskDate}</div>
+                <div class="myDate">${pretifyDate(item.newNoteDate)}</div>
                 <div class="myTime">${item.taskTime}</div> 
                 </div>
            `;
     });
     tableData.innerHTML = toHtml;
 };
-/*const removeNote = () => {
-const element = document.getElementById("myNote");
-  element.remove();
-  localStorage.removeItem;("myData");
-};*/
-
-
-
-function deleteItem() {
+  const removeNote = ()=> {
   const element = document.getElementById("myNote");
   element.remove();
- localStorage.removeItem("myData");
-}
+  localStorage.removeItem("myData");
+};
+const pretifyDate = () => {
+   var myDate= new Date(taskDate.value).toLocaleDateString();
+   var newDate = myDate.split("/");
+   return `${newDate[1]}/${newDate[0]}/${newDate[2]}`;
+};
+const newNoteDate =document.createElement("div");
+newNoteDate.classList.add("date");
+newNoteDate.innerHTML = pretifyDate(taskDate);
+newNote.element.appendChild(newNoteDate);
