9047babad4ce34a7a88bb8c28511cb20668ebddf
Daniel Gapso <dandan11110000@gmail.com>
2023-01-15 18:02:42 +0200

    ⚒️

--- js/js.js
+++ js/js.js
@@ -13,7 +13,7 @@ const makeTable = () => {
     myTasks.push(note);
     document.getElementById("myForm").reset();
     localStorage.setItem("myData", JSON.stringify(myTasks));
-    newNoteDate()
+
     const tableData = document.getElementById("mySticky");
     var toHtml = "";
     myTasks.map((item) => {
@@ -21,7 +21,7 @@ const makeTable = () => {
                 <div id="myNote">
                 <span class="material-symbols-outlined" onclick="removeNote()" id="deleteButton">close</span>
                 <div class="myWrite">${item.taskWrite}</div>
-                <div class="myDate">${pretifyDate(item.newNoteDate)}</div>
+                <div class="myDate">${item.taskDate}</div>
                 <div class="myTime">${item.taskTime}</div> 
                 </div>
            `;
@@ -33,12 +33,3 @@ const makeTable = () => {
   element.remove();
   localStorage.removeItem("myData");
 };
-const pretifyDate = () => {
-   var myDate= new Date(taskDate.value).toLocaleDateString();
-   var newDate = myDate.split("/");
-   return `${newDate[1]}/${newDate[0]}/${newDate[2]}`;
-};
-const newNoteDate =document.createElement("div");
-newNoteDate.classList.add("date");
-newNoteDate.innerHTML = pretifyDate(taskDate);
-newNote.element.appendChild(newNoteDate);
