ecf4c44b37938d92a6543e8f41ad1c0daa50468f
Daniel Gapso <dandan11110000@gmail.com>
2023-02-28 12:48:23 +0200

    ⚒️

--- js/js.js
+++ js/js.js
@@ -1,23 +1,23 @@
-var myTasks = [];
+var myTasks = [];//an array to store the tasks
 
-const newDate = (taskDate) => {
-  const ilDate = taskDate.split("-");
-  return `${ilDate[2]}/${ilDate[1]}/${ilDate[0]}`;
+const newDate = (taskDate) => {//a new date function to show the date of the task as dd//mm/yy
+  const ilDate = taskDate.split("-");//to split the date by the -
+  return `${ilDate[2]}/${ilDate[1]}/${ilDate[0]}`;//return the date as dd/mm/yy
 };
-const removeNote = (event) => {
+const removeNote = (event) => {//an event that happen when the user clicked on the X button on the note to remove the note
   let element = event.target.parentNode;
   let index = Array.from(element.parentNode.children).indexOf(element);
   myTasks.splice(index, 1);
   element.remove();
   localStorage.setItem("myData", JSON.stringify(myTasks));
 };
-const checkForData = () => {
+const checkForData = () => {//a function to check for data and store the data in localStorage
   if (localStorage.getItem("myData")) {
     myTasks = JSON.parse(localStorage.getItem("myData"));
-    insertData();
+    insertData();//invoke insert data function
   }
 };
-const insertData = () => {
+const insertData = () => {//a function that gets the input data from the user and displays the data at the screen
   const divData = document.getElementById("mySticky");
   var toHtml = "";
   myTasks.map((item) => {
@@ -33,7 +33,7 @@ const insertData = () => {
   divData.innerHTML = toHtml;
 };
 checkForData();
-const makeDiv = () => {
+const makeDiv = () => {//a function that gets the input data from the user and adds a note with the data to the array
   const note = new Object();
   note.taskWrite = document.getElementById("taskWrite").value;
   note.taskDate = document.getElementById("taskDate").value;
